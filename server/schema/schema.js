14431a788f103bab57f207bcb5ac9756ad5cc061
reanzi <nziku99@gmail.com>
2018-09-19 14:58:29 +0300

    Relation both ways, done

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -78,6 +78,18 @@ const RootQuery = new GraphQLObjectType({
         // code to get data from db/source
         return _.find(authors, { id: args.id });
       }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books;
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return authors;
+      }
     }
   }
 });
