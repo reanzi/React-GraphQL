f501cd8b1c1e493dd2941a8facfbf29e26dea010
reanzi <nziku99@gmail.com>
2018-09-19 17:29:26 +0300

    Added mLab connection and mongoose, works

--- server/app.js
+++ server/app.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// connect to mLab db
+mongoose.connect(
+  "mongodb://reanzi:reanzi1@ds161102.mlab.com:61102/react_graph",
+  { useNewUrlParser: true }
+);
+mongoose.connection
+  .on("error", console.error.bind(console, "connection error:"))
+  .once("open", () => {
+    console.log("connected to the MongoDB");
+  });
+
 app.use(
   "/graphql",
   graphqlHTTP({
