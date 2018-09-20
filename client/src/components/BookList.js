2b875bfdaa71b047de878674ac054c4392044ddc
reanzi <nziku99@gmail.com>
2018-09-20 16:17:21 +0300

    Displaying books on the front-end

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -13,13 +13,24 @@ const getBooksQuery = gql`
 `;
 
 class BookList extends Component {
+  displayBooks() {
+    var data = this.props.data;
+    if (data.loading) {
+      return <div>Loading books ...</div>;
+    } else {
+      return data.books.map(book => {
+        return (
+          <li key={book.id}>
+            {book.name} - <span>{book.genre}</span>
+          </li>
+        );
+      });
+    }
+  }
   render() {
-    console.log(this.props);
     return (
       <div>
-        <ul id="book-list">
-          <li>Book Name</li>
-        </ul>
+        <ul id="book-list">{this.displayBooks()}</ul>
       </div>
     );
   }
